e640e207a106905d2e52ce4286aa47cb1007a760
Edvaldo Leite <eguilhermeleite@gmail.com>
2023-05-20 12:47:51 -0300

    target07

--- src/test/java/com/anna/AnnaSampleCustomerRegistrationApplicationTests.java
+++ src/test/java/com/anna/AnnaSampleCustomerRegistrationApplicationTests.java
@@ -1,4 +1,4 @@
-/*package com.anna;
+package com.anna;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,4 +11,3 @@ class AnnaSampleCustomerRegistrationApplicationTests {
 	}
 
 }
-*/
